890b767dcd4e7d12a81233acf072dbb17a7314df
springrx <kangchunrx@163.com>
2024-01-30 21:01:14 +0800

    change: mysql pwd

--- src/config/config.default.ts
+++ src/config/config.default.ts
@@ -28,8 +28,8 @@ export default (appInfo: EggAppInfo) => {
     host: "127.0.0.1",
     port: "3306",
     username: "root",
-    //  password: "curidemo",
-    password: "52KangChun",
+    password: "curidemo",
+    // password: "52KangChun",
     timezone: "+08:00",
   };
 
